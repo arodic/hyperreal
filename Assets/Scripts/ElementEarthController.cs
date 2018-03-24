64e7b4037e082e523b275f2badccd2445045d446
AKI RODIC <aleksandar.xyz@gmail.com>
2018-03-24 18:58:05 +0100

    Added element controller.

---
+++ Assets/Scripts/ElementEarthController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementEarthController : MonoBehaviour {
+
+  private ElementController controller;
+
+	void Start () {
+    controller = GetComponent<ElementController>();
+	}
+
+	void Update () {
+    if (controller) {
+
+    }
+	}
+
+}
---
+++ Assets/Scripts/ElementWindController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementWindController : MonoBehaviour {
+
+  private ElementController controller;
+  private ParticleSystem particles;
+
+	void Start () {
+    controller = GetComponent<ElementController>();
+    particles = GetComponent<ParticleSystem>();
+	}
+
+	void Update () {
+    if (controller && particles) {
+      var emission = particles.emission;
+      emission.rateOverTime = 1000f * controller.fade;
+    }
+	}
+
+}
