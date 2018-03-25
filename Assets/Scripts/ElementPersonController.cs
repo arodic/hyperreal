a9612199db58556a9e08990fd17478159952f807
AKI RODIC <aleksandar.xyz@gmail.com>
2018-03-25 15:07:43 +0200

    Added glitch.

--- Assets/Scripts/ElementPersonController.cs
+++ Assets/Scripts/ElementPersonController.cs
@@ -9,13 +9,10 @@ public class ElementPersonController : MonoBehaviour
     public AudioSource monologueAudioSource;
     public Material girlMat;
 
-    
-
-
-
     void Update()
     {
         if(controller.active) monologueAudioSource.enabled = true;
+        if(girlMat && controller) girlMat.SetFloat("_Glitch", 1.0f - controller.fade);
     }
 
 }
