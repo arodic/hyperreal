44edc3c995712d37306dafc7857af70ed953b4f7
DudleyHK <dudleydawes@hotmail.co.uk>
2018-03-25 11:18:05 +0100

    audio files ellemetns

---
+++ Assets/Scripts/DeactiveWhileRunning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactiveWhileRunning : MonoBehaviour 
+{
+    private ElementController m_elementController;
+
+
+
+    private void Awake()
+    {
+        m_elementController = GetComponent<ElementController>();
+    }
+
+
+
+    private void Update()
+    {
+        if(m_elementController.active)
+        {
+            print("Fade OUT");
+        }
+        else
+        {
+            Debug.Log("FADE IN");
+        }
+
+    }
+
+}
--- Assets/Scripts/ElementEarthController.cs
+++ Assets/Scripts/ElementEarthController.cs
@@ -2,30 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElementEarthController : MonoBehaviour {
+public class ElementEarthController : MonoBehaviour
+{
+    private ElementController controller;
+    public RTIvyController ivy;
+    public Material mat1;
+    public Material mat2;
+    public AudioSource audioSource; 
 
-  private ElementController controller;
-  public RTIvyController ivy;
-  public Material mat1;
-  public Material mat2;
-
-	void Start () {
-    controller = GetComponent<ElementController>();
-	}
-
-	void Update () {
-    if (controller && ivy) {
-      if(!controller.active) {
-        ivy.elapsedTime = 0.0f;
-      }
-      ivy.IvyEnabled = controller.active;
-      if(mat1) {
-        mat1.SetColor("_Color", new Color(1f,1f,0f,controller.fade));
-      }
-      if(mat2) {
-        mat2.SetColor("_Color", new Color(1f,1f,0f,Mathf.Pow(controller.fade, 5.0f)));
-      }
+    void Start()
+    {
+        controller = GetComponent<ElementController>();
+        audioSource = GetComponent<AudioSource>();
     }
-	}
+
+    void Update()
+    {
+        if(controller && ivy)
+        {
+            audioSource.volume = 1;
+
+            if(!controller.active)
+            {
+                ivy.elapsedTime = 0.0f;
+                audioSource.volume = 0f;
+            }
+
+            
+            ivy.IvyEnabled = controller.active;
+            if(mat1)
+            {
+                mat1.SetColor("_Color", new Color(1f, 1f, 0f, controller.fade));
+            }
+            if(mat2)
+            {
+                mat2.SetColor("_Color", new Color(1f, 1f, 0f, Mathf.Pow(controller.fade, 5.0f)));
+            }
+        }
+    }
 
 }
